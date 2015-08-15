a7d4dad711f0d161459b15394743c7c2e5ea2813
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2015-08-14 23:09:32 -0700

    migrating to new uranium.

---
+++ ubuild.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+
+
+def main(build):
+    build.packages.install(".", develop=True)
+
+
+def test(build):
+    main(build)
+    build.packages.install("jedi")
+    build.packages.install("sphinx")
+    build.packages.install("pytest")
+    build.packages.install("pytest-cov")
+    pytest = os.path.join(build.root, "bin", "py.test")
+    subprocess.call([pytest, "--cov", "flask_transmute", "flask_transmute/tests"])
+
+
+def _download_swagger_ui(build):
+    pass
+    # _script: "uscripts/download_swagger_ui.py"
