ad1b54e3d6d12440059337e117fb4ef60059ee58
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2016-02-04 23:43:41 -0800

    publishing new flask-transmute version.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ install_requires = [
 tests_require = []
 
 setup(name='flask-transmute',
-      version='0.2.8',
+      version='0.2.10b',
       description='a flask plugin to generate routes from objects.',
       long_description=open(README_PATH).read(),
       author='Yusuke Tsutsumi',
