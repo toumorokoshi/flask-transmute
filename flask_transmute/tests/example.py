bce2483adcf689701e1782dda5c9ef99904dc89c
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2017-07-10 20:41:30 -0700

    minor: updating example.

--- flask_transmute/tests/example.py
+++ flask_transmute/tests/example.py
@@ -1,15 +1,42 @@
-from flask_transmute import route, annotate, Response
+from flask_transmute import (
+    route, annotate, Response, APIException
+)
 import flask_transmute
+from schematics.models import Model
+from schematics.types import StringType, IntType
 from flask import Flask, Blueprint
 
 app = Flask(__name__)
 
+class Person(Model):
+    name = StringType()
+    number_of_cats = IntType()
 
+
+CAT_PEOPLE = {
+    "george": Person({"name": "george",
+                      "number_of_cats": 2})
+}
+
+@flask_transmute.route(app, paths='/cat_person')
+#                        methods=["POST"])
+def get_cat_person(name: str) -> Person:
+    """ find and return a cat person """
+    if name in CAT_PEOPLE:
+        return CAT_PEOPLE[name]
+    raise APIException("no cat person found", code=404)
+
+
+# alternatively
 @flask_transmute.route(app, paths='/multiply')
-@annotate({"left": int, "right": int, "return": int})
-def multiply(left, right):
+def multiply(left: int, right: int) -> int:
+    """ multiply two integers """
     return left * right
 
+# type hint equivalent in Py2
+multiply.__annotations__ = {
+    "left": int, "right": int, "return": int
+}
 
 @route(app, paths='/exception')
 def exception():
